80420bc24ef688e7be24e954a67031b6e778790f
shivsundar3009 <shivsundar3009@gmail.com>
2024-01-27 22:14:28 +0530

    06bgColorChanger completed

--- 06bgColorChanger/src/App.jsx
+++ 06bgColorChanger/src/App.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+ 
+   const [color, setColor] = useState('olive')
+
+
   return (
-    <div className='bg-blue-200'>Basic Setup + Tailwind</div>
+    <div className='h-screen w-full text-white '
+         style={{backgroundColor: color}}
+    >
+     
+      <div className='bg-[#2a9d8f] flex justify-center gap-5 p-5 w-full max-h-20'>
+          <div className='bg-red-700 p-2 rounded-xl cursor-pointer min-w-20 text-center hover:bg-white hover:text-red-700 hover:border-red-700 hover:border-2 ' 
+          onClick={() => setColor('red')}
+          >
+             red
+          </div>
+          <div className='bg-yellow-300 p-2 rounded-xl cursor-pointer min-w-20 text-center hover:bg-white hover:text-yellow-300 hover:border-yellow-300 hover:border-2 '
+          onClick={() => setColor('yellow')}
+          >
+             yellow
+          </div>
+          <div className='bg-blue-700 p-2 rounded-xl cursor-pointer min-w-20 text-center hover:bg-white hover:text-blue-700 hover:border-blue-700 hover:border-2 '
+          onClick={() => setColor('blue')}
+          >
+             blue
+          </div>
+          <div className='bg-black p-2 rounded-xl cursor-pointer min-w-20 text-center hover:bg-white hover:text-black hover:border-black hover:border-2 '
+          onClick={() => setColor('black')}
+          >
+             black
+          </div>
+          <div className='bg-green-700 p-2 rounded-xl cursor-pointer min-w-20 text-center hover:bg-white hover:text-green-700 hover:border-green-700 hover:border-2 '
+          onClick={() => setColor('green')}
+          >
+             green
+          </div>
+        
+          
+         
+     </div>
+    
+    </div>
   )
 }
 
